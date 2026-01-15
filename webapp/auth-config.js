d33bb7f24fd26a7110567b949b78a8655af205d7
Max Shirokov <maxshmax666@users.noreply.github.com>
2026-01-15 11:09:26 +0500

    chore(auth): set google client id

--- webapp/auth-config.js
+++ webapp/auth-config.js
@@ -1,4 +1,4 @@
 window.PUBLIC_AUTH_CONFIG = {
-  telegramBotUsername: "",
-  googleClientId: "",
+  telegramBotUsername: "PizzaNT_bot",
+  googleClientId: "164532542405-v2c5cor5e02fjuh2qfofgpmeodroa3i4.apps.googleusercontent.com",
 };
